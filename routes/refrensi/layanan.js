4baf847f66b71674430063b30c0bbf4d4e68a307
chand <candra@ilcs.co.id>
2020-06-29 00:30:57 +0700

    update

--- routes/refrensi/layanan.js
+++ routes/refrensi/layanan.js
@@ -4,5 +4,6 @@ const controller = require('../../controller/index');
 const {check, validationResult} = require('express-validator');
 
 router.get('/', controller.layanan.getAll);
+router.get('/get', controller.layanan.get);
 
 module.exports = router;
